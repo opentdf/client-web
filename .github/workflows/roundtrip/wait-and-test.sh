f1c3890065536c51f810b91f44e5eff8c972e2a5
David Mihalcik <dmihalcik@virtru.com>
2022-08-01 16:59:59 -0400

    try harder

--- .github/workflows/roundtrip/wait-and-test.sh
+++ .github/workflows/roundtrip/wait-and-test.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
 APP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+ROOT_DIR="$(cd "${APP_DIR}/../../.." >/dev/null && pwd)"
 APP="${APP_DIR}/encrypt-decrypt.sh"
+
+echo "whereami? $(pwd) -> $APP_DIR -> $ROOT_DIR"
+echo "whatishere"
+ls -l
+
+_configure_app() {
+  cli_version=$(cd "${ROOT_DIR}/cli" && node -p "require('./package.json').version")
+
+  if [ -f "${APP_DIR}/opentdf-cli-${cli_version}.tgz" ]; then
+    cp "opentdf-cli-${cli_version}.tgz" ./cli/
+    (
+      cd "${APP_DIR}" || exit 1
+      npm uninstall @opentdf/cli && npm ci && npm i "../../../cli/opentdf-cli-${cli_version}.tgz"
+    )
+  else
+    npm i
+  fi
+}
 
 _wait-for() {
   echo "[INFO] In retry loop for quickstarted opentdf backend..."
@@ -10,7 +29,8 @@ _wait-for() {
     if sh "${APP}"; then
       return 0
     fi
-    if [[ $i == $limit ]]; then
+    if [[ $i == "$limit" ]]; then
+      echo "[WARN] Breaking loop as we are at limit"
       break
     fi
     sleep_for=$((10 + i * i * 2))
@@ -21,4 +41,5 @@ _wait-for() {
   exit 1
 }
 
+_configure_app
 _wait-for
